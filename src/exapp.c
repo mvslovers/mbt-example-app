18747769b93a6ff2de5620c797e78d88c9c8b969
Mike Großmann <mike.grossmann@betasystems.com>
2026-03-05 15:46:05 +0100

    exlib added as dependency

--- src/exapp.c
+++ src/exapp.c
@@ -1,8 +1,15 @@
 /* exapp.c — example application */
+#include "exlib.h"
 #include <stdio.h>
 
-int main()
-{
-    printf("EXAPP: Hello from mbt-example-app!\n");
-    return 0;
+int main() {
+  int rc = 0;
+
+  int sum = 0;
+
+  sum = exlib_add(42, 0);
+
+  printf("EXAPP: Hello from mbt-example-app. The answer is always %d !!!\n");
+
+  return 0;
 }
